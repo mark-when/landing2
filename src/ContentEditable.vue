5f68b023f0a67d6d708604499dd7bdf173f79966
Rob Koch <kochrt@gmail.com>
2024-09-02 17:05:41 -0700

    ContentEditable

---
+++ src/ContentEditable.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const model = defineModel<string>();
+</script>
+
+<template>
+  <div
+    contenteditable="true"
+    class="bg-zinc-50 rounded-s h-48 font-mono text-sm p-2 whitespace-pre-wrap"
+    @keyup="model = ($event.target as HTMLDivElement).textContent || ''"
+  >
+    {{ model }}
+  </div>
+</template>
+
+<style scoped></style>
