8bee63fdc1d3491a172ede2622a3188b73660322
Rob Koch <kochrt@gmail.com>
2024-09-04 06:22:55 -0700

    fix examples editing

--- src/ContentEditable.vue
+++ src/ContentEditable.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
 const model = defineModel<string>();
+const content = ref<HTMLDivElement>();
+onMounted(() => {
+  content.value!.textContent = model.value;
+})
 </script>
 
 <template>
   <div
+    ref="content"
     contenteditable="true"
     class="bg-zinc-50 rounded-s h-48 font-mono text-sm p-2 whitespace-pre-wrap"
-    @keyup="model = ($event.target as HTMLDivElement).textContent || ''"
+    @input="model = ($event.target as HTMLDivElement).textContent || ''"
   >
-    {{ model }}
   </div>
 </template>
 
