bca1339667587ae03c9e1cea6e18da983a59e913
Rob Koch <kochrt@gmail.com>
2024-09-07 21:08:46 -0700

    another attempt at redirects

---
+++ server/middleware/[[index]].ts
@@ -0,0 +1,26 @@
+export default defineEventHandler(async (r) => {
+  const path = r.context.params?.index;
+  if (path) {
+    if (Array.isArray(path)) {
+      if (path.length) {
+        const last = path[path.length - 1];
+        if (last) {
+          if (
+            last.includes(".") &&
+            !last.endsWith(".mw") &&
+            !last.endsWith(".json")
+          ) {
+            return;
+          }
+        } else {
+          return;
+        }
+      } else {
+        return;
+      }
+    } else if (path.includes(".")) {
+      return;
+    }
+  }
+  return sendRedirect(r, `https://meridiem.markwhen.com/${path}`);
+});
